7e68e50da911c86693a94f42477f7fecd3ef3c66
Gabriel Catani <gabrielm.catani@gmail.com>
2023-06-23 15:20:07 -0300

    Server done! Onward to crete simple version of client for testing.

--- SocketServer/src/main/java/edu/home42/sockets/app/Main.java
+++ SocketServer/src/main/java/edu/home42/sockets/app/Main.java
@@ -3,17 +3,31 @@ package edu.home42.sockets.app;
 import edu.home42.sockets.config.SocketsApplicationConfig;
 import edu.home42.sockets.models.User;
 import edu.home42.sockets.repositories.UsersRepositoryImpl;
+import edu.home42.sockets.server.Server;
 import edu.home42.sockets.services.UsersServiceImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SocketsApplicationConfig.class);
-        User usr = new User();
-        usr.setUsername("Tiranossauro");
-        usr.setPassword("dinossauro");
-
         UsersServiceImpl usrService = context.getBean(UsersServiceImpl.class);
-        usrService.signUp(usr);
+
+        Server server = new Server(usrService, 6666);
+        try {
+            server.init();
+            server.listenAndAccept();
+            server.sendMessage("Hello from Server!");
+            User usr = new User();
+            usr.setUsername(server.receiveMessage());
+            usr.setPassword(server.receiveMessage());
+            usrService.signUp(usr);
+            server.sendMessage("Successful");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ SocketServer/src/main/java/edu/home42/sockets/server/Server.java
@@ -0,0 +1,56 @@
+package edu.home42.sockets.server;
+
+import edu.home42.sockets.services.UsersServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private UsersServiceImpl usersService;
+    private ServerSocket serverSocket;
+    private Integer port;
+    private InputStreamReader inputStream;
+    private BufferedReader inputBuffer;
+
+    private PrintWriter outputWriter;
+
+    @Autowired
+    public Server(UsersServiceImpl usersService, Integer port) {
+        this.usersService = usersService;
+        this.port = port;
+    }
+
+    public void init() throws IOException {
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
+    public void listenAndAccept() throws IOException {
+        Socket clientSocket = this.serverSocket.accept();
+        inputStream = new InputStreamReader(clientSocket.getInputStream());
+        inputBuffer = new BufferedReader(inputStream);
+        outputWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+    }
+
+    public void sendMessage(String msg) {
+        this.outputWriter.println(msg);
+    }
+
+    public String receiveMessage() throws IOException {
+        StringBuilder msg = new StringBuilder();
+        String line = new String();
+        while ((line = this.inputBuffer.readLine()) != null) {
+            msg.append(line);
+        }
+
+        return msg.toString();
+    }
+
+    public void close() throws IOException {
+        this.outputWriter.close();
+        this.inputBuffer.close();
+        this.inputBuffer.close();
+        this.serverSocket.close();
+    }
+}
