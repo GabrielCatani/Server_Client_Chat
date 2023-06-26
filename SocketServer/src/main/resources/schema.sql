dadae5b904854975db363cdfac7d2fecef933188
Gabriel Catani <gabrielm.catani@gmail.com>
2023-06-26 12:10:05 -0300

    Incremented schema to include new db for storing messages;

--- SocketServer/src/main/resources/schema.sql
+++ SocketServer/src/main/resources/schema.sql
@@ -1,6 +1,12 @@
-CREATE SCHEMA chat_users;
-CREATE TABLE chat_users.users (
+CREATE SCHEMA IF NOT EXISTS chat_users;
+CREATE TABLE IF NOT EXISTS chat_users.users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100),
     password VARCHAR(100)
 );
+CREATE TABLE IF NOT EXISTS chat_users.messages (
+   id SERIAL PRIMARY KEY,
+   sender VARCHAR(100),
+   message VARCHAR(250),
+   timestamp TIMESTAMP
+)
